586518196d93d7e9fb57d485f7615a989ff1f3ad
SleepingDaemon <wiredgear@gmail.com>
2021-05-08 10:56:16 -0400

    Created a player script with the ability to move when left clicking around the nav mesh

---
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour 
+{
+    [SerializeField] private NavMeshAgent _agent = null;
+
+    private void Update()
+    {
+        //if left click
+        if(Input.GetMouseButtonDown(0))
+        {
+            //cast a ray from the mouse position
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            if(Physics.Raycast(ray, out hit))
+            {
+                //debug the floor position hit
+                Debug.Log(hit.point);
+
+                //create object at floor position to know the floor has been hit.
+                GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                sphere.transform.position = hit.point;
+                //Instantiate(sphere, hit.point, Quaternion.identity);
+
+                _agent.SetDestination(hit.point);
+            }
+        }
+    }
+    
+}
