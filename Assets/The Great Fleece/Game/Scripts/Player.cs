b6afc2c9627202c2a179280846ffbe89ce478f7c
SleepingDaemon <wiredgear@gmail.com>
2021-05-09 19:48:09 -0400

    Added idle and walk animations to the player

--- Assets/The Great Fleece/Game/Scripts/Player.cs
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -6,23 +6,28 @@ using UnityEngine.AI;
 public class Player : MonoBehaviour 
 {
     [SerializeField] private NavMeshAgent _agent = null;
+    [SerializeField] private Animator _anim = null;
+    private Vector3 _direction;
+
+    private RaycastHit hit;
 
     private void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             
             if(Physics.Raycast(ray, out hit))
             {
-                Debug.Log(hit.point);
+                _anim.SetBool("isWalking", true);
+                //_agent.SetDestination(hit.point);
+                _agent.destination = hit.point;
+            } 
+        }
 
-                GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                sphere.transform.position = hit.point;
-
-                _agent.SetDestination(hit.point);
-            }
+        if(Vector3.Distance(transform.position, hit.point) < 1)
+        {
+            _anim.SetBool("isWalking", false);
         }
     }
     
