2236612feab1dd7dfe4717082e5836d678d5ff8a
SleepingDaemon <wiredgear@gmail.com>
2021-05-09 12:27:32 -0400

    Added two scripts: CameraTrigger, LookAtPlayer. These control the logic to change camera angles upon triggers and look at the player when moving

---
+++ Assets/The Great Fleece/Game/Scripts/CameraTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour 
+{
+    [SerializeField] private Transform _camAngle = null;
+
+	private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Trigger Activated");
+            var cam = Camera.main;
+
+            _camAngle.gameObject.SetActive(true);
+            cam.transform.position = _camAngle.position;
+            cam.transform.rotation = _camAngle.rotation;
+        }
+    }
+}
--- Assets/The Great Fleece/Game/Scripts/Player.cs
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -9,22 +9,17 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        //if left click
         if(Input.GetMouseButtonDown(0))
         {
-            //cast a ray from the mouse position
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             
             if(Physics.Raycast(ray, out hit))
             {
-                //debug the floor position hit
                 Debug.Log(hit.point);
 
-                //create object at floor position to know the floor has been hit.
                 GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
                 sphere.transform.position = hit.point;
-                //Instantiate(sphere, hit.point, Quaternion.identity);
 
                 _agent.SetDestination(hit.point);
             }
